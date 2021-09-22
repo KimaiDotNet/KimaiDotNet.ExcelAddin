795f4d9be28b47a75dec95fd499d8fafc18b3794
MarkZither <mark.burton@zither-it.co.uk>
2021-09-22 23:00:00 +0200

    Add build project

---
+++ build/Build.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.MSBuild;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
+
+[ShutdownDotNetAfterServerBuild]
+[CheckBuildProjectConfigurations]
+[GitHubActions(
+    "dotnet-core",
+    GitHubActionsImage.WindowsLatest,
+    OnPushBranches = new[] { MainBranch, DevelopBranch, ReleaseBranchPrefix + "/*", VersionBranchPrefix + "*" },
+    InvokedTargets = new[] { nameof(Compile) },
+    ImportGitHubTokenAs = nameof(GitHubToken),
+    ImportSecrets =
+        new[]
+        {
+            nameof(NuGetApiKey)
+        })]
+partial class Build : NukeBuild
+{
+    /// <summary>
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+    /// </summary>
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [CI] private readonly GitHubActions GitHubActions;
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
+    [GitVersion] readonly GitVersion GitVersion;
+
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+    AbsolutePath TestsDirectory => RootDirectory / "tests";
+    AbsolutePath OutputDirectory => RootDirectory / "output";
+
+    private const string MainBranch = "main";
+    private const string DevelopBranch = "develop";
+    private const string ReleaseBranchPrefix = "release";
+    private const string VersionBranchPrefix = "v";
+    private bool IsOriginalRepository => GitRepository != null && GitRepository.Identifier == "KimaiDotNet/KimaiDotNet.ApiClient";
+
+    private string NuGetPackageSource => "https://api.nuget.org/v3/index.json";
+    private string GitHubPackageSource => $"https://nuget.pkg.github.com/{GitHubActions.GitHubRepositoryOwner}/index.json";
+    string Source => IsOriginalRepository ? NuGetPackageSource : GitHubPackageSource;
+
+    [Parameter] private readonly string NuGetApiKey;
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+            EnsureCleanDirectory(OutputDirectory);
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            MSBuild(s => s
+                .SetTargetPath(Solution)
+                .SetTargets("Restore"));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            MSBuild(s => s
+                .SetTargetPath(Solution)
+                .SetTargets("Rebuild")
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .SetMaxCpuCount(Environment.ProcessorCount)
+                .SetNodeReuse(IsLocalBuild));
+        });
+
+}
